b107dfce4f810c386d0f2aee08e7a401623573f4
nanisam07 <jgmcsamuelvictor@gmail.com>
2025-06-30 06:17:32 +0530

    its 6:17

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -1,16 +1,29 @@
+'use client';
+
 import { blogPosts } from '@/app/blog/blogData';
 import BlogCard from './BlogCard';
+import { motion } from 'framer-motion';
 
 export default function BlogList() {
   return (
     <div className="grid md:grid-cols-3 gap-8">
-      {blogPosts.map((post) => {
-        // Normalize the image property
+      {blogPosts.map((post, index) => {
         const normalizedPost = {
           ...post,
-          image: post.image ?? post.Images ?? '',
+          image: post.image || '', 
         };
-        return <BlogCard key={post.slug} blog={normalizedPost} />;
+
+        return (
+          <motion.div
+            key={post.slug}
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.1 }}
+            viewport={{ once: true }}
+          >
+            <BlogCard blog={normalizedPost} />
+          </motion.div>
+        );
       })}
     </div>
   );
