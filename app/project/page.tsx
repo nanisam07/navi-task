4e846839c54b471195c2d38d0f073a88baab13df
nanisam07 <jgmcsamuelvictor@gmail.com>
2025-07-06 16:07:12 +0530

    added dumb page for project

---
+++ app/project/page.tsx
@@ -0,0 +1,10 @@
+// app/project/page.tsx
+'use client';
+
+export default function ProjectLandingPage() {
+  return (
+    <div className="text-white min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-3xl font-bold">Welcome to Our Projects</h1>
+    </div>
+  );
+}
